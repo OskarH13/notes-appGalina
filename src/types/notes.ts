2595be4bb4dac14e51d83341e7a8470c0e42e906
OskarH13 <161576666+OskarH13@users.noreply.github.com>
2024-04-23 10:55:11 +0200

    adding Tuesday work

--- src/types/notes.ts
+++ src/types/notes.ts
@@ -1,13 +1,21 @@
 export class Note {
-  id: number;
-  title: string;
-  content: string;
-  user: string;
+    id: number;
+    title: string;
+    content: string;
+    user: string;
 
-  constructor(id: number, title: string, content: string, user: string) {
-    this.id = id;
-    this.title = title;
-    this.content = content;
-    this.user = user;
+    constructor(id: number, title: string, content: string, user: string) {
+      this.id = id;
+      this.title = title;
+      this.content = content;
+      this.user = user;
+    }
   }
+
+/**
+ * Represents the raw data structure of notes data, containing an array of notes,
+ * to extract the notes from the raw JSON object.
+ */
+export type NotesRaw = {
+  notes: Note[]
 }
